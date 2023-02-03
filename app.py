8bf77ae302042e8b794530e4196f25346fc83c40
Nishant Gerald <nishant.gerald@gmail.com>
2023-02-02 21:36:18 -0500

    changed to accept additional file formats

--- app.py
+++ app.py
@@ -21,10 +21,24 @@ menu = ["Upload", "Metadata"]
 choice = st.sidebar.selectbox("Select an option", menu)
 
 # Upload file and read it into a Pandas dataframe
-uploaded_file = st.file_uploader("Choose a CSV file", type=["csv"], key='file')
+uploaded_file = st.file_uploader("Choose a CSV file", type=["csv", "tsv", "json", "xlsx", "xml"], key='file')
 data = None
 if uploaded_file is not None:
-    data = pd.read_csv(uploaded_file)
+    file_extension = uploaded_file.name.split(".")[-1].lower()
+    if file_extension == "csv":
+        data = pd.read_csv(uploaded_file)
+    elif file_extension == "tsv":
+        data = pd.read_csv(uploaded_file, sep="\t")
+    elif file_extension == "json":
+        data = pd.read_json(uploaded_file)
+    elif file_extension == "xlsx":
+        data = pd.read_excel(uploaded_file)
+    elif file_extension == "xml":
+        # You will need to install the 'xmltodict' library to read XML files
+        import xmltodict
+        with uploaded_file as file:
+            data = pd.DataFrame.from_dict(xmltodict.parse(file.read()))
+
 
 def main():
     if choice == "Upload":
