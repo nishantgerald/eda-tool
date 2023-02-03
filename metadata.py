7dfa93419c3dfffcef710bba72954ba55d85f834
Nishant Gerald <nishant.gerald@gmail.com>
2023-02-03 11:10:39 -0500

    display metadata in columns and converted df to tables

--- metadata.py
+++ metadata.py
@@ -13,27 +13,39 @@ def show_metadata(data):
         st.write("No data loaded.")
         return
 
-    # Show number of rows
-    st.write("Number of rows:", data.shape[0])
+    # Place number of rows metric and number of columns metric side by side
+    col1, col2 = st.columns(2)
+    
+    # Show number of rows in col1 and number of columns in col2
+    with col1:
+        # Show number of rows
+        st.metric("Number of rows", data.shape[0])
 
-    # Show number of columns
-    st.write("Number of columns:", data.shape[1])
+    with col2:
+        # Show number of columns
+        st.metric("Number of columns", data.shape[1])
+
+    
 
     # Show summary statistics
-    st.write("Summary statistics:")
-    st.write(data.describe())
+    st.subheader("Summary statistics:")
+    st.table(data.describe())
 
-    # Show missing values count
-    st.write("Missing values count:")
-    st.write(data.isna().sum())
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        # Show missing values count
+        st.subheader("Missing values count:")
+        st.table(data.isna().sum())
 
-    # Show unique values count
-    st.write("Unique values count:")
-    unique_values_count = {col: len(data[col].unique()) for col in data.columns}
-    st.dataframe(pd.DataFrame(list(unique_values_count.items()), columns=["Column", "Unique Count"]))
+    with col2:
+        # Show unique values count
+        st.subheader("Unique values count:")
+        unique_values_count = {col: len(data[col].unique()) for col in data.columns}
+        st.table(pd.DataFrame(list(unique_values_count.items()), columns=["Column", "Unique Count"]))
 
-    # Show most frequent values
-    st.write("Most frequent values:")
-    most_frequent_values = {col: data[col].value_counts().head(1).reset_index().rename(columns={'index':'value', col:'count'}) for col in data.columns}
-    most_frequent_values_df = pd.concat(most_frequent_values.values(), keys=most_frequent_values.keys(), axis=1)
-    st.dataframe(most_frequent_values_df.T)
+    with col3:
+        # Show most frequent values
+        st.subheader("Most frequent values:")
+        most_frequent_values = {col: data[col].value_counts().head(1).reset_index().rename(columns={'index':'value', col:'count'}) for col in data.columns}
+        most_frequent_values_df = pd.concat(most_frequent_values.values(), keys=most_frequent_values.keys(), axis=1)
+        st.table(most_frequent_values_df.T)
